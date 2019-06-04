575e5e9dd7cb50afa198ce1e0db965d48347aa9d
Noctea <38964329+Noctea@users.noreply.github.com>
2019-06-05 00:58:43 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js")
+const bot = new Discord.Client()
+
+const Command = require("./command/command")
+const Ping = require("./command/ping")
+const Infos = require("./command/infos")
+const Help = require("./command/help")
+const Staff = require("./command/staff")
+
+bot.on("ready", function() {
+    bot.user.setActivity("RokzyBot | .help")
+})
+
+bot.login(process.env.TOKEN)
+
+bot.on("guildMemberAdd", member => {
+    member.guild.channels.find("name", "logs-bvn").send(`${member} vient de rejoindre le Discord, bienvenue !!`)
+})
+
+bot.on("message", message => {
+
+    Ping.parse(message)
+    Infos.parse(message)
+    Help.parse(message)
+    Staff.parse(message)
+})
