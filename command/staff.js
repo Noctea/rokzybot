12cf595bea4b709828fdfd4a0e01fff73af9360a
Noctea <38964329+Noctea@users.noreply.github.com>
2019-06-05 01:18:58 +0200

    Add files via upload

---
+++ command/staff.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js")
+const Command = require("./command")
+
+const prefix = (".")
+
+module.exports = class Staff extends Command {
+
+    static match(message) {
+        if (message.content === prefix + "staff") {
+            return true
+        }
+    }
+
+    static action(message) {
+
+        message.channel.send("<@&530730102762373123>")
+
+        var staff = new Discord.RichEmbed()
+            .setTitle("Demande de STAFF :")
+            .setDescription("Un membre du <@&530730102762373123> te contactera dès que possible.")
+            .setThumbnail("https://www.nativitychurch.org/wp-content/uploads/2018/02/staff-box-1280x450.jpg")
+            .setColor("0xBF00FF")
+            .setFooter(`Demande de ${message.author.username}`)
+
+        message.channel.sendEmbed(staff)
+        .then(function(message) {
+            message.react("✅")
+        })
+    }
+}
