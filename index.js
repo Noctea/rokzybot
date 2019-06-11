d6b157e2ec2e9655a5ce371bf57e0229ec033424
Noctea <contact@noctea.tk>
2019-06-11 20:24:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Ping = require("./command/ping")
 const Infos = require("./command/infos")
 const Help = require("./command/help")
 const Staff = require("./command/staff")
-const Canard = require("./command/canard")
+const Caneton = require("./command/canard")
 
 bot.on("ready", function() {
     bot.user.setActivity("RokzyBot | .help")
@@ -24,5 +24,5 @@ bot.on("message", message => {
     Infos.parse(message)
     Help.parse(message)
     Staff.parse(message)
-    Canard.parse(message)
+    Caneton.parse(message)
 })
