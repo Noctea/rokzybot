35060a971b991aa9d96ef0693fad4e4206f93b0b
Noctea <contact@noctea.tk>
2019-06-13 03:46:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,116 @@
-const Discord = require("discord.js")
-const bot = new Discord.Client()
+const {
+    Client,
+    Collection
+} = require("discord.js");
+const {
+    promisify
+} = require("util");
+const readdir = promisify(require("fs").readdir);
+const Enmap = require("enmap");
+const klaw = require("klaw");
+const path = require("path");
 
-const Command = require("./command/command")
-const Ping = require("./command/ping")
-const Infos = require("./command/infos")
-const Help = require("./command/help")
-const Staff = require("./command/staff")
-const Caneton = require("./command/caneton")
+class RokZyBot extends Client {
+    constructor(options) {
+        super(options);
 
-bot.on("ready", function() {
-    bot.user.setActivity("RokzyBot | .help")
-})
+        this.config = require("./config.js");
 
-bot.login(process.env.TOKEN)
+        this.commands = new Collection();
+        this.aliases = new Collection();
 
-bot.on("guildMemberAdd", member => {
-    member.guild.channels.find("name", "🆕-bienvenue").send(`${member}, bienvenue sur le Discord destiné à la communauté de RokZy ! Passe un bon moment ici ! :purple_heart: `)
-})
+        this.settings = new Enmap({
+            name: "settings",
+            cloneLevel: "deep",
+            fetchAll: false,
+            autoFetch: true
+        });
 
-bot.on("message", message => {
+        this.logger = require("./modules/logger");
+        this.wait = require("util").promisify(setTimeout);
+    }
 
-    Ping.parse(message)
-    Infos.parse(message)
-    Help.parse(message)
-    Staff.parse(message)
-    Caneton.parse(message)
-})
+    //Permission
+
+    permLevel(message) {
+        let permlvl = 0;
+
+        const permOrder = this.config.permLevels.slice(0).sort((p, c) => (p.level < c.level ? 1 : -1));
+
+        while (permOrder.length) {
+            const currentLevel = permOrder.shift();
+            if (message.guild && currentLevel.guildOnly) continue;
+            if (currentLevel.check(message)) {
+                permlvl = currentLevel.level;
+                break;
+            }
+        }
+        return permlvl;
+    }
+
+    loadCommand(commandPath, commandName) {
+        try {
+            const props = new(require(`${commandPath}${path.sep}${commandName}`))(
+                this
+            );
+            this.logger.log(`Chargement de la commande: ${props.help.name}`, "log");
+            props.conf.location = commandPath;
+            if (props.init) {
+                props.init(this);
+            }
+            this.commands.set(props.help.name, props);
+            props.conf.aliases.forEach(alias => {
+                this.aliases.set(alias, props.help.name);
+            });
+            return false;
+        } catch (e) {
+            return `Impossible de charger la commande ${commandName}: ${e}`;
+        }
+    }
+
+    getSettings(guild) {
+        const defaults = this.config.defaultSettings || {};
+        const guildData = this.settings.get(guild.id) || {};
+        const returnObject = {};
+        Object.keys(defaults).forEach(key => {
+            returnObject[key] = guildData[key] ? guildData[key] : defaults[key];
+        })
+        return returnObject;
+    }
+}
+
+const client = new RokZyBot();
+console.log(client.config.permLevels.map(p => `${p.level}: ${p.name}`));
+
+//Fonction d'initialisation
+
+const init = async () => {
+    klaw("./commands").on("data", item => {
+        const cmdFile = path.parse(item.path);
+        if (!cmdFile.ext || cmdFile.ext !== ".js") return;
+        const response = client.loadCommand(cmdFile.dir, `${cmdFile.name}${cmdFile.ext}`
+        );
+        if  (response) client.logger.error(response);
+    });
+
+    //Récupération des événements
+    const evtFiles = await readdir("./events");
+    client.logger.log(`Chargement de ${evtFiles.length} événements.`, "log");
+    evtFiles.forEach(file => {
+        const eventName = file.split(".")[0];
+        client.logger.log(`Chargement de l'événement: ${eventName}`);
+        const event = new (require(`./events/${file}`))(client);
+        client.on(eventName, (...args) => event.run(...args));
+        delete require.cache[require.resolve(`./events/${file}`)];
+    });
+
+    client.levelCache = {};
+    for (let i = 0; i < client.config.permLevels.length; i++) {
+        const thisLevel = client.config.permLevels[i];
+        client.levelCache[thisLevel.name] = thisLevel.level;
+    }
+
+    client.login(process.env.TOKEN);
+};
+
+init();
