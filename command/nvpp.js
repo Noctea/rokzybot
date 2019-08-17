c6ef2c0fca5678ea6d504fea059d884795fd025c
Noctea <contact@noctea.tk>
2019-08-17 20:45:00 +0200

    Create nvpp.js

---
+++ command/nvpp.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js")
+const Command = require("./command")
+
+const prefix = (".")
+
+module.exports = class Nvpp extends Command {
+
+    static match(message) {
+        if (message.content === prefix + "nvpp") {
+            return true
+        }
+    }
+
+    static action(message) {
+
+        var nvpp = new Discord.RichEmbed()
+            .setTitle("LA NOUVELLE PP DU <@585604187941765146> !")
+            .setDescription("<@490943350996729887> dans toute sa splendeur !")
+            .setThumbnail("https://cdn.discordapp.com/attachments/286230717367713792/612355077356912651/rokzychasse2.jpg")
+            .setColor("0xFF7B00")
+            .setFooter("Abonne toi aux réseaux de rokzyy la blg pour avoir des photos de très bonne qualité comme celle-ci :)")
+
+        message.channel.sendEmbed(nvpp)
+        .then(function(message) {
+            message.react("😍")
+        })
+    }
+}
